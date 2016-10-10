ce322d5d1b0557485b719089d9cb3ae9a7814aba
vadimcastro <vcastro@albany.edu>
2016-10-09 21:19:48 -0400

    Creating of Node class and AlphaInsert method

---
+++ src/DoublyLinkedList.java
@@ -0,0 +1,28 @@
+/**
+ * Created by vadimcastro on 10/9/16.
+ */
+public class DoublyLinkedList {
+
+    public class Node {
+
+        public Node(int val, Node n){
+            int value = val;
+            Node next = n;
+        }
+    }
+
+    /**
+     * AlphaInsert method inserts a new node into an existing list in alphabetical order
+     * @param list the list for the node to be inserted into
+     */
+
+    public void AlphaInsert(Node list){
+
+    }
+
+
+
+
+
+
+}
